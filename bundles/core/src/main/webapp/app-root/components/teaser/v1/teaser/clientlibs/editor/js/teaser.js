653543bda63a18073a4ac2facbd46c36231c7961
Stefan Seifert <stefanseifert@users.noreply.github.com>
2021-03-18 22:47:11 +0100

    update with files from core components 2.15.0

--- bundles/core/src/main/webapp/app-root/components/teaser/v1/teaser/clientlibs/editor/js/teaser.js
+++ bundles/core/src/main/webapp/app-root/components/teaser/v1/teaser/clientlibs/editor/js/teaser.js
@@ -27,84 +27,89 @@
         var dialogContent = $dialogContent.length > 0 ? $dialogContent[0] : undefined;
 
         if (dialogContent) {
-            titleTuple = new CheckboxTextfieldTuple(dialogContent, titleCheckboxSelector, titleTextfieldSelector);
-            descriptionTuple = new CheckboxTextfieldTuple(dialogContent, descriptionCheckboxSelector, descriptionTextfieldSelector, true);
 
-            var $linkURLField = $dialogContent.find(linkURLSelector);
-            if ($linkURLField) {
-              var $linkURLFoundationField = $linkURLField.adaptTo("foundation-field");
-              if ($linkURLFoundationField) {
-                linkURL = $linkURLFoundationField.getValue();
-                $linkURLField.on("change", function() {
-                    linkURL = $linkURLFoundationField.getValue();
-                    retrievePageInfo($dialogContent);
-                });
-              }
-            }
-
-            var $actionsEnabledCheckbox = $dialogContent.find(actionsEnabledCheckboxSelector);
-            if ($actionsEnabledCheckbox.size() > 0) {
-                actionsEnabled = $actionsEnabledCheckbox.adaptTo("foundation-field").getValue() === "true";
-                $actionsEnabledCheckbox.on("change", function(e) {
-                    actionsEnabled = $(e.target).adaptTo("foundation-field").getValue() === "true";
-                    toggleInputs($dialogContent);
-                    retrievePageInfo($dialogContent);
-                });
-
-                var $actionsMultifield = $dialogContent.find(actionsMultifieldSelector);
-                $actionsMultifield.on("change", function(event) {
-                    var $target = $(event.target);
-                    if ($target.is("coral-multifield") && event.target.items && event.target.items.length === 0) {
-                        actionsEnabled = false;
-                        $actionsEnabledCheckbox.adaptTo("foundation-field").setValue(false);
-                        toggleInputs($dialogContent);
-                    } else if ($target.is("foundation-autocomplete")) {
-                        updateText($target);
-                    }
-                    retrievePageInfo($dialogContent);
+            var rteInstance = $(descriptionTextfieldSelector).data("rteinstance");
+            // wait for the description textfield rich text editor to signal start before initializing.
+            // Ensures that any state adjustments made here will not be overridden.
+            if (rteInstance && rteInstance.isActive) {
+                init(e, $dialog, $dialogContent, dialogContent);
+            } else {
+                $(descriptionTextfieldSelector).on("editing-start", function() {
+                    init(e, $dialog, $dialogContent, dialogContent);
                 });
             }
-
-            toggleInputs($dialogContent);
-            retrievePageInfo($dialogContent);
         }
     });
+
+    // Initialize all fields once both the dialog and the description textfield RTE have loaded
+    function init(e, $dialog, $dialogContent, dialogContent) {
+        titleTuple = new CheckboxTextfieldTuple(dialogContent, titleCheckboxSelector, titleTextfieldSelector, false);
+        descriptionTuple = new CheckboxTextfieldTuple(dialogContent, descriptionCheckboxSelector, descriptionTextfieldSelector, true);
+        retrievePageInfo($dialogContent);
+
+        var $linkURLField = $dialogContent.find(linkURLSelector);
+        if ($linkURLField.length) {
+            linkURL = $linkURLField.adaptTo("foundation-field").getValue();
+            $linkURLField.on("change", function() {
+                linkURL = $linkURLField.adaptTo("foundation-field").getValue();
+                retrievePageInfo($dialogContent);
+            });
+        }
+
+        var $actionsEnabledCheckbox = $dialogContent.find(actionsEnabledCheckboxSelector);
+        if ($actionsEnabledCheckbox.size() > 0) {
+            actionsEnabled = $actionsEnabledCheckbox.adaptTo("foundation-field").getValue() === "true";
+            $actionsEnabledCheckbox.on("change", function(e) {
+                actionsEnabled = $(e.target).adaptTo("foundation-field").getValue() === "true";
+                toggleInputs($dialogContent);
+                retrievePageInfo($dialogContent);
+            });
+
+            var $actionsMultifield = $dialogContent.find(actionsMultifieldSelector);
+            $actionsMultifield.on("change", function(event) {
+                var $target = $(event.target);
+                if ($target.is("coral-multifield") && event.target.items && event.target.items.length === 0) {
+                    actionsEnabled = false;
+                    $actionsEnabledCheckbox.adaptTo("foundation-field").setValue(false);
+                    toggleInputs($dialogContent);
+                } else if ($target.is("foundation-autocomplete")) {
+                    updateText($target);
+                }
+                retrievePageInfo($dialogContent);
+            });
+        }
+        toggleInputs($dialogContent);
+    }
 
     function toggleInputs(dialogContent) {
         var $actionsMultifield = dialogContent.find(actionsMultifieldSelector);
         var linkURLField = dialogContent.find(linkURLSelector).adaptTo("foundation-field");
         var actions = $actionsMultifield.adaptTo("foundation-field");
-        if (actionsEnabled) {
-            if (linkURLField) {
-              linkURLField.setDisabled(true);
-            }
-            if (actions) {
-              actions.setDisabled(false);
-            }
-            if ($actionsMultifield.size() > 0) {
-                var actionsMultifield = $actionsMultifield[0];
-                if (actionsMultifield.items.length < 1) {
-                    var newMultifieldItem = new Coral.Multifield.Item();
-                    actionsMultifield.items.add(newMultifieldItem);
-                    Coral.commons.ready(newMultifieldItem, function(element) {
-                        var linkField = $(element).find("[data-cmp-teaser-v1-dialog-edit-hook='actionLink']");
-                        if (linkField) {
-                            linkField.val(linkURL);
-                            linkField.trigger("change");
-                        }
-                    });
-                } else {
-                    toggleActionItems($actionsMultifield, false);
+        if (linkURLField && actions) {
+            if (actionsEnabled) {
+                linkURLField.setDisabled(true);
+                actions.setDisabled(false);
+                if ($actionsMultifield.size() > 0) {
+                    var actionsMultifield = $actionsMultifield[0];
+                    if (actionsMultifield.items.length < 1) {
+                        var newMultifieldItem = new Coral.Multifield.Item();
+                        actionsMultifield.items.add(newMultifieldItem);
+                        Coral.commons.ready(newMultifieldItem, function(element) {
+                            var linkField = $(element).find("[data-cmp-teaser-v1-dialog-edit-hook='actionLink']");
+                            if (linkField) {
+                                linkField.val(linkURL);
+                                linkField.trigger("change");
+                            }
+                        });
+                    } else {
+                        toggleActionItems($actionsMultifield, false);
+                    }
                 }
+            } else {
+                linkURLField.setDisabled(false);
+                actions.setDisabled(true);
+                toggleActionItems($actionsMultifield, true);
             }
-        } else {
-            if (linkURLField) {
-              linkURLField.setDisabled(false);
-            }
-            if (actions) {
-              actions.setDisabled(true);
-            }
-            toggleActionItems($actionsMultifield, true);
         }
     }
 
