97aaddb44effed2a000718dbbe6dfaf7cab6a254
Stefan Seifert <stefanseifert@users.noreply.github.com>
2026-06-18 12:55:43 +0200

    fix code warning

--- bundles/core/src/main/java/io/wcm/wcm/core/components/impl/util/ComponentFeatureImageResolver.java
+++ bundles/core/src/main/java/io/wcm/wcm/core/components/impl/util/ComponentFeatureImageResolver.java
@@ -120,7 +120,7 @@ public class ComponentFeatureImageResolver {
    * @return Media
    */
   public @NotNull Media buildMedia() {
-    Media media = mediaHandler.invalid();
+    Media media;
 
     // special handling if imageFromPageImage is not set at all - check if image from resource is valid, otherwise fallback to image from page.
     boolean useImageFromPageImage = imageFromPageImage != null && imageFromPageImage;
